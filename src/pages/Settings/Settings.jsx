80b27cd45305dd66355e4464929312e754963117
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-05-27 21:42:07 -0400

    started the Settings

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -2,17 +2,20 @@ import styles from "./Settings.module.css";
 import { LeftNavigation } from "../../components/LeftNavigation/LeftNavigation";
 import { Link } from "react-router-dom";
 import BlueButton from "../../components/BlueButton/BlueButton";
-import Frances from "../../assets/Images/Network/Frances.png";
-import Martha from "../../assets/Images/Network/Martha.png";
-import Matilda from "../../assets/Images/Network/Matilda.png";
-import Luna from "../../assets/Images/Network/Luna.jpg";
-import Samantha from "../../assets/Images/Network/Samantha.jpg";
-import Crystal from "../../assets/Images/Network/Crystal.jpg";
+import Lara from "../../assets/Images/ProfileImages/Lara.png";
+import ClearButton from "../../components/ClearButton/ClearButton";
 
 export default function Settings() {
   return (
     <main className={styles.main}>
       <LeftNavigation />
+
+      <body className={styles.Body}>
+        <div className={styles.UserProfile}>
+          <img src={Lara} alt="Lara" className={styles.UserImage} />
+          <ClearButton text="Comment" />
+        </div>
+      </body>
     </main>
   );
 }
