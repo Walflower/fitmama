4066d0bbbdb871d9e2011799091fdcd6490c9e29
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-05-27 23:49:14 -0400

    few changes to about us section

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -21,7 +21,7 @@ export default function AboutUs() {
             <div className={styles.CheckBoxTitle}>
               <b>Welcome to The Fit Mama Hub!</b>
             </div>
-            <div className={styles.heckBoxTitle}>
+            <div className={styles.CheckBoxTitle}>
               At The Fit Mama Hub, we understand the unique challenges that come
               with balancing motherhood, work, and personal wellness. Our
               mission is to create a supportive and engaging community where
@@ -29,7 +29,47 @@ export default function AboutUs() {
               experiences, and achieve their health and fitness goals together.
             </div>
           </div>
+        </section>
+
+        <section className={styles.TextFields}>
           <div className={styles.QuestionnaireTitle}>Our Vision</div>
+          <div className={styles.CheckBoxContainer}>
+            <div className={styles.CheckBoxTitle}>
+              We aim to empower moms to reclaim their personal time and
+              prioritize their well-being through a holistic approach to fitness
+              and health. By fostering a community of like-minded mothers, we
+              provide the resources, support, and motivation needed to navigate
+              the demanding yet rewarding journey of motherhood.
+            </div>
+          </div>
+        </section>
+        <section className={styles.TextFields}>
+          <div className={styles.QuestionnaireTitle}>What we offer </div>
+          <div className={styles.CheckBoxContainer}>
+            <div className={styles.CheckBoxTitle}>
+              Community Support: An inclusive forum where you can connect with
+              other moms, share tips, and find encouragement.
+            </div>
+            <div className={styles.CheckBoxTitle}>
+              Expert Advice: Access to professional advice from experts in
+              postnatal fitness, nutrition, and wellness.(Coming soon )
+            </div>
+            <div className={styles.CheckBoxTitle}>
+              Personalized Fitness Programs: Tailored workout plans that fit
+              into your busy schedule, helping you stay active and
+              healthy.(Coming soon )
+            </div>
+            <div className={styles.CheckBoxTitle}>
+              We are committed to making fitness and wellness accessible and
+              enjoyable for all moms. Our platform is designed to accommodate
+              the diverse needs of mothers, ensuring you can find the right
+              balance between caring for your family and taking care of
+              yourself. Join The Fit Mama Hub today and become part of a
+              community that understands your journey and supports your goals.
+              Together, we can inspire each other to lead healthier, happier
+              lives while setting a positive example for our children.
+            </div>
+          </div>
         </section>
       </section>
     </main>
