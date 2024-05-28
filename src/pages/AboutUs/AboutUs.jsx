b53dfc31104f284022a620ddecd1b7973cef1a4f
Nyakuma Wal <Nyakuma.wal@gmail.com>
2024-05-27 23:41:56 -0400

    About us sett up

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -13,6 +13,25 @@ export default function AboutUs() {
   return (
     <main className={styles.main}>
       <LeftNavigation />
+
+      <section className={styles.Body}>
+        <section className={styles.TextFields}>
+          <div className={styles.QuestionnaireTitle}>About Us</div>
+          <div className={styles.CheckBoxContainer}>
+            <div className={styles.CheckBoxTitle}>
+              <b>Welcome to The Fit Mama Hub!</b>
+            </div>
+            <div className={styles.heckBoxTitle}>
+              At The Fit Mama Hub, we understand the unique challenges that come
+              with balancing motherhood, work, and personal wellness. Our
+              mission is to create a supportive and engaging community where
+              mothers of young children (ages 0-5) can connect, share
+              experiences, and achieve their health and fitness goals together.
+            </div>
+          </div>
+          <div className={styles.QuestionnaireTitle}>Our Vision</div>
+        </section>
+      </section>
     </main>
   );
 }
