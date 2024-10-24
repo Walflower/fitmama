5e4ca9068a05211f90fbb843dbf1550bfa6ca4b0
Nyajal Wal <nyajal.w@gmail.com>
2024-10-24 16:55:43 -0400

    error validation in the front end is working, but it's not submitting the form

--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -2,64 +2,132 @@ import styles from "./SignUp.module.css";
 import ShortTextField from "../../components/ShortTextField/ShortTextField";
 import CreateAccountImage from "../../assets/Images/signup-image.svg";
 import SignUpWithGoogle from "../../components/SignUpWithGoogle/SignUpWithGoogle";
-import SignUpBlueButton from "../../components/SignUpBlueButton/SigupBlueButton";
+// import SignUpBlueButton from "../../components/SignUpBlueButton/SigupBlueButton";
 import EyePasswordImage from "../../assets/Images/eye password logo.png";
 import SignUpWithGoogleImage from "../../assets/Images/Google Logo.jpg";
 import { Link } from "react-router-dom";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Validation } from "./SignUpValidation";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 export default function SignUp() {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    phone_number: "",
-    password: "",
-  });
+  // const [values, setValues] = useState({
+  //   first_name: null,
+  //   last_name: null,
+  //   email: null,
+  //   password_hash: null,
+  //   location: null,
+  // });
 
   const [errors, setErrors] = useState([]);
+  const formRef = useRef(null);
 
-  const handleInput = (event) => {
-    setValues((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
+  //Generating an unique user_id
+  const generateUserId = () => {
+    return uuidv4();
   };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setErrors(Validation(values));
+
+  // const handleInput = (event) => {
+  //   setValues((prev) => ({
+  //     ...prev,
+  //     [event.target.name]: event.target.value,
+  //   }));
+  // };
+
+  //.....
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   setErrors(Validation(values));
+  // };
+  //......
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Access form values using formRef
+    const form = formRef.current;
+    const values = {
+      first_name: form.elements.first_name.value,
+      last_name: form.elements.last_name.value,
+      email: form.elements.email.value,
+      password_hash: form.elements.password_hash.value,
+      location: form.elements.location.value,
+    };
+
+    //Validate the form values
+    const validationErrors = Validation(values);
+    setErrors(validationErrors);
+
+    //if no errors
+    if (Object.keys(validationErrors).length === 0) {
+      await postNewUser(values);
+      form.reset(); // Reset the form after successful submission
+    } else {
+      alert("Failed to add user, you have some errors in your form.");
+    }
   };
+
+  //ADDING (POST) a new users
+  const postNewUser = async (values) => {
+    try {
+      const newUser = {
+        user_id: generateUserId(),
+        ...values,
+      };
+
+      console.log(newUser);
+      const postResponse = await axios.post(
+        "http://localhost:8080/users",
+        newUser
+      );
+      console.log(postResponse);
+    } catch (error) {
+      console.error("Error adding new user:", error);
+    }
+  };
+
   return (
     <div className={styles.CreateAccountContainer}>
       <div className={styles.CreateAccountImageContainer}>
-        <img className={styles.CreateAccountImage} src={CreateAccountImage} />
+        <img
+          className={styles.CreateAccountImage}
+          src={CreateAccountImage}
+          alt="create an account"
+        />
       </div>
 
       <div className={styles.SignUpContainer}>
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} ref={formRef}>
           <div className={styles.SignUpTitleContainer}>
             <p className={styles.SignUpTitle}>Create An Account</p>
           </div>
 
           <div className={styles.TextFieldContainer}>
             <ShortTextField
-              placeholder="Enter Your Name"
-              name="name"
-              onChange={handleInput}
+              placeholder="First Name"
+              name="first_name"
+              type="text"
             />
-            {errors.name && (
-              <span className={styles.danger}>{errors.name}</span>
+            {errors.first_name && (
+              <span className={styles.danger}>{errors.first_name}</span>
             )}
           </div>
 
           <div className={styles.TextFieldContainer}>
             <ShortTextField
-              placeholder="Enter Your Email"
-              type="email"
-              name="email"
-              onChange={handleInput}
+              placeholder="Last Name"
+              name="last_name"
+              type="text"
             />
+            {errors.last_name && (
+              <span className={styles.danger}>{errors.last_name}</span>
+            )}
+          </div>
+
+          <div className={styles.TextFieldContainer}>
+            <ShortTextField placeholder="Email" type="email" name="email" />
             {errors.email && (
               <span className={styles.danger}>{errors.email}</span>
             )}
@@ -67,30 +135,31 @@ export default function SignUp() {
 
           <div className={styles.TextFieldContainer}>
             <ShortTextField
-              placeholder="Enter Your Phone Number"
-              type="tel"
-              name="phone_number"
-              onChange={handleInput}
+              placeholder="Password"
+              image={EyePasswordImage}
+              type="password"
+              name="password_hash"
             />
-            {errors.phone_number && (
-              <span className={styles.danger}>{errors.phone_number}</span>
+            {errors.password_hash && (
+              <span className={styles.danger}>{errors.password_hash}</span>
             )}
           </div>
 
           <div className={styles.TextFieldContainer}>
             <ShortTextField
-              placeholder="Enter Your Password"
-              image={EyePasswordImage}
-              type="password"
-              name="password"
-              onChange={handleInput}
+              placeholder="Location"
+              name="location"
+              type="text"
             />
-            {errors.password && (
-              <span className={styles.danger}>{errors.password}</span>
+            {errors.location && (
+              <span className={styles.danger}>{errors.location}</span>
             )}
           </div>
 
-          <SignUpBlueButton text="Sign Up" type="submit" />
+          <button type="submit" className={styles.Button}>
+            Sign Up
+          </button>
+          {/* <SignUpBlueButton text="Sign Up" type="submit" /> */}
 
           <div className={styles.OrWitheContainer}>
             <div className={styles.Line}></div>
--- src/pages/Signup/SignUpValidation.jsx
+++ src/pages/Signup/SignUpValidation.jsx
@@ -1,32 +1,39 @@
 export function Validation(values) {
   let error = {};
-  const email_pattern = /^[^\s@+@[^\s@]+\.[^\s@]+$/;
+  // const email_pattern = /^[^\s@+@[^\s@]+\.[^\s@]+$/;
+  //trying this email pattern
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
-  const phone_pattern =
-    /^\+?(\d{1,3})?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)?(\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9})$/;
+  // const phone_pattern =
+  //   /^\+?(\d{1,3})?[-.\s]?(\(?\d{1,4}\)?[-.\s]?)?(\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9})$/;
 
   /**the errors aren't really working, if input name is still shows the error message. fix */
-  if (!values.name) {
-    error.name = "Name should not be empty";
+  if (!values.user_id) {
+    error.user_id = "User id not created correctly";
+  }
+  if (!values.first_name || !values.first_name.trim()) {
+    error.first_name = "Enter your first name";
+  }
+  if (!values.last_name || !values.last_name.trim()) {
+    error.last_name = "Enter your last name";
   }
 
-  if (!values.email) {
+  if (!values.email || !values.email.trim()) {
     error.email = "Enter your email";
   } else if (!email_pattern.test(values.email)) {
     error.email = "Invalid email";
   }
 
-  if (!values.phone_number) {
-    error.phone_number = "Enter your phone number";
-  } else if (!phone_pattern.test(values.phone_number)) {
-    error.phone_number = "Invalid phone number";
+  if (!values.password_hash) {
+    error.password_hash = "Enter a password";
+  } else if (!password_pattern.test(values.password_hash)) {
+    error.password_hash =
+      "Password must contain at least 8 characters, including an uppercase letter, a lowercase letter, and a number";
   }
 
-  if (!values.password) {
-    error.password = "Enter a password";
-  } else if (!password_pattern.test(values.password)) {
-    error.password =
-      "Password must contain at least 8 characters, including an uppercase letter, a lowercase letter, and a number";
+  if (!values.location) {
+    error.location = "Select a location";
   }
   return error;
 }
